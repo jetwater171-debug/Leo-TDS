030b7f7aa03557a487c0f3ede7ed30a3efd8981d
Daniel Vygolov <rattack@gmail.com>
2025-09-18 18:16:09 +0400

    refactoring

--- abtest.php
+++ abtest.php
@@ -1,47 +1,68 @@
 <?php
 require_once __DIR__ . '/cookies.php';
+require_once __DIR__ . '/logging.php';
+require_once __DIR__ . '/campaign.php';
 
-function select_item(array $items, bool $save_user_flow = false, string $item_type = 'landing', bool $is_folder = true): array
+class AbTest
 {
-    $item = '';
-
-    if ($save_user_flow) {
-        $item = get_saved_item($item_type, $items, $is_folder);
+    private Campaign $campaign;
+    public function __construct(Campaign $campaign)
+    {
+        $this->campaign = $campaign;
     }
 
-    if ($item === '') {
-        $item = get_random_item($items);
+    public function select_item_by_index(array $items, int $index): string
+    {
+        if (!isset($items[$index]))
+            $index = rand(0, count($items) - 1);
+        return $items[$index];
     }
 
-    set_cookie($item_type, $item, '/');
-    return [$item, array_search($item, $items)];
-}
-
-function get_saved_item(string $item_type, array $items, bool $is_folder): string
-{
-    $item = get_cookie($item_type);
-
-    if (!in_array($item, $items, true) || ($is_folder && !is_folder_valid($item))) {
-        return '';
+    public function select_folder(array $folders, string $folder_type): array
+    {
+        return $this->select_item($folders, $folder_type, true);
     }
 
-    return $item;
-}
+    public function select_item(array $items, string $item_type, bool $is_folder): array
+    {
+        if (empty($items))
+            add_error_log("No items found for $item_type in campaign {$this->campaign->campaignId}!", false, true);
 
-function is_folder_valid(string $item): bool
-{
-    return is_dir(__DIR__ . '/' . $item);
-}
+        $item = '';
 
-function get_random_item(array $items): string
-{
-    $random_index = rand(0, count($items) - 1);
-    $item = $items[$random_index];
-    return $item;
-}
+        if ($this->campaign->saveUserFlow) {
+            $item = $this->get_saved_item($item_type, $items, $is_folder);
+        }
 
-function select_item_by_index(array $items, int $index): string
-{
-    if (!isset($items[$index])) $index = rand(0, count($items) - 1);
-    return $items[$index];
+        if ($item === '') {
+            $item = $this->get_random_item($items);
+        }
+
+        set_cookie($item_type, $item);
+        return [$item, array_search($item, $items)];
+    }
+
+    private function get_saved_item(string $item_type, array $items, bool $is_folder): string
+    {
+        $item = get_cookie($item_type);
+
+        if (!in_array($item, $items, true) || ($is_folder && !$this->is_folder_valid($item))) {
+            return '';
+        }
+
+        return $item;
+    }
+
+    private function is_folder_valid(string $item): bool
+    {
+        return is_dir(__DIR__ . '/' . $item);
+    }
+
+    private function get_random_item(array $items): string
+    {
+        $random_index = rand(0, count($items) - 1);
+        $item = $items[$random_index];
+        return $item;
+    }
+
 }
