1a5da59bcd85eb1a13618ae033f8f0b3a68de169
Daniel Vygolov <rattack@gmail.com>
2026-02-26 12:10:04 +0400

    jszip для группировки в скачиваемом xlsx

--- admin/scripts.php
+++ admin/scripts.php
@@ -1,7 +1,7 @@
 <?php
-require_once __DIR__.'/../paths.php';
-$jsFsPath = __DIR__.'/js';
-$jsPath = get_cloaker_path().'js';
+require_once __DIR__ . '/../paths.php';
+$jsFsPath = __DIR__ . '/js';
+$jsPath = get_cloaker_path() . 'js';
 ?>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="<?=$jsPath?>/jquery.modal.min.js"></script>
@@ -10,7 +10,7 @@ $jsPath = get_cloaker_path().'js';
 <script src="<?=$jsPath?>/flatpickr.js"></script>
 <script src="<?=$jsPath?>/luxon.min.js"></script>
 <script src="<?=$jsPath?>/xlsx.full.min.js"></script>
-<script src="<?=$jsPath?>/tabulator.js?v=<?= filemtime($jsFsPath.'/tabulator.js') ?>"></script>
-<script src="<?=$jsPath?>/campeditor.js?v=<?= filemtime($jsFsPath.'/campeditor.js') ?>"></script>
-<script src="<?=$jsPath?>/header.js?v=<?= filemtime($jsFsPath.'/header.js') ?>"></script>
-<!-- <script src="<?=$jsPath?>/bootstrap.bundle.min.js"></script> -->
+<script src="<?=$jsPath?>/jszip.min.js"></script>
+<script src="<?= $jsPath ?>/tabulator.js?v=<?= filemtime($jsFsPath . '/tabulator.js') ?>"></script>
+<script src="<?= $jsPath ?>/campeditor.js?v=<?= filemtime($jsFsPath . '/campeditor.js') ?>"></script>
+<script src="<?= $jsPath ?>/header.js?v=<?= filemtime($jsFsPath . '/header.js') ?>"></script>
